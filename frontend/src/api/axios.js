1264c67dd1bdaeb6f4d03d44c498c526b78edb33
DEVANG SINGH <devang9890@gmail.com>
2026-03-26 21:25:56 +0530

    updated

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://127.0.0.1:8000"
+  baseURL: "https://medguard-final-1.onrender.com"
 });
 
 API.interceptors.request.use((req) => {
